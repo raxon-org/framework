8002aedfce3ce076a63b22fc1a59a31116d2936f
remco van der Velde <remco.pc@outlook.com>
2024-09-10 22:30:45 +0200

    rename to RaXon

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -1108,6 +1108,7 @@ class Route extends Data {
             $object->data(App::ROUTE, $route);
         }
         $host = strtolower($object->config('host.name'));
+        d($host);
         if(empty($host) && Core::is_cli()){
             Route::framework($object);
             $node = new Node($object);
