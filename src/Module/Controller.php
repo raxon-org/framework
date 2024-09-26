89a466fe8d3b7e16c0a8c2cac052c91ac5ea334b
remco van der Velde <remco.pc@outlook.com>
2024-09-26 23:48:48 +0200

    empty array test

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -414,7 +414,7 @@ class Controller {
         }
         if(empty($url)){
             if($logger_error){
-                $object->logger($logger_error)->error('Cannot find view file ('. $name . ') url: ' . $url);
+                $object->logger($logger_error)->error('Cannot find view file ('. $name . ') ', [$list]);
             }
             if (
                 $config->data(Config::DATA_FRAMEWORK_ENVIRONMENT) === Config::MODE_INIT ||
