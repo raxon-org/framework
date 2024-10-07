dba653f2cea02487f6a244989a7d72143dc0fe3b
remco van der Velde <remco.pc@outlook.com>
2024-10-07 19:27:43 +0200

    sets

--- src/Debug.php
+++ src/Debug.php
@@ -60,9 +60,9 @@ if(!function_exists('breakpoint')){
                 array_key_exists('trace', $options) &&
                 $options['trace'] === true
             ){
-                Cli::read('input-hidden',$trace[0]['file'] . ':' . $trace[0]['line'] . PHP_EOL . $export . PHP_EOL . 'Press '. Cli::info('enter') . ' to continue or ' . Cli::error('ctrl-c') . ' to break...');
+                Cli::read('input-hidden',$trace[0]['file'] . ':' . $trace[0]['line'] . PHP_EOL . $export . PHP_EOL . 'Press '. Cli::info('enter') . ' to continue or ' . Cli::error('ctrl-c') . ' to break...' . PHP_EOL);
             } else {
-                Cli::read('input-hidden', $export . PHP_EOL . 'Press '. Cli::info('enter') . ' to continue or ' . Cli::error('ctrl-c') . ' to break...');
+                Cli::read('input-hidden', $export . PHP_EOL . 'Press '. Cli::info('enter') . ' to continue or ' . Cli::error('ctrl-c') . ' to break...' . PHP_EOL);
             }
         }
     }
