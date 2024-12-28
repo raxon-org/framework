068f4712ec948d59ad165382b42460a482742071
remco van der Velde <remco.pc@outlook.com>
2024-12-28 20:34:47 +0100

    tada

--- src/Module/File.php
+++ src/Module/File.php
@@ -281,7 +281,7 @@ class File {
         }
         $exist = File::exist($source);
         if($exist === false){
-            throw new FileMoveException('Source file doesn\'t exist...');
+            throw new FileMoveException('Source ('. $source . ') file doesn\'t exist...');
         }
         $exist = File::exist($destination);
         if(
