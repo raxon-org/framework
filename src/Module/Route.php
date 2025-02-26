82533d5992c051c7fd871f063d96e3faa789c56c
remco van der Velde <remco.pc@outlook.com>
2025-02-26 21:22:55 +0100

    test

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -1108,11 +1108,11 @@ class Route extends Data {
             $object->data(App::ROUTE, $route);
         }
         $host = strtolower($object->config('host.name'));
-        d($host);
         if(empty($host) && Core::is_cli()){
             Route::framework($object);
             $node = new Node($object);
             $role_system = $node->role_system();
+            d($role_system);
             $response = false;
             if($role_system){
                 $response = $node->list(
