a4620480b1c8e1d528e9344be40a9ee8321dd18b
remco van der Velde <remco.pc@outlook.com>
2025-02-26 19:30:03 +0100

    test

--- src/App.php
+++ src/App.php
@@ -103,6 +103,7 @@ class App extends Data {
         App::is_cli();
         require_once __DIR__ . '/Debug.php';
         require_once __DIR__ . '/Error.php';
+        ddd('tesy');
         Config::configure($this);
         Logger::configure($this);
         Host::configure($this);
