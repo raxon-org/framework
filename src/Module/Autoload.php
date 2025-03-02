4b8574b9eb59587e5a7b2609794051f423fef9d2
remco van der Velde <remco.pc@outlook.com>
2025-03-02 11:45:30 +0100

    make functions like new parser (traits)

--- src/Module/Autoload.php
+++ src/Module/Autoload.php
@@ -485,36 +485,38 @@ class Autoload {
             $object->config('cache.autoload.url.directory_separator') &&
             $object->config('cache.autoload.url.directory_pop_or_shift')
         ){
-            $load = $item['directory'] . $item['file'];
-            $load_directory = dirname($load);
-            $load = basename($load) . '.' . Autoload::EXT_PHP;
-            $load_compile = Autoload::name_reducer(
-                $object,
-                $load,
-                $object->config('cache.parse.url.name_length'),
-                $object->config('cache.parse.url.name_separator'),
-                $object->config('cache.parse.url.name_pop_or_shift')
-            );
-            if(str_contains($load_compile, '_')){
-                $data[] = $object->config('autoload.cache.compile') . $load_compile;
+            foreach($item['directory'] as $directory){
+                $load = $item['directory'] . $item['file'];
+                $load_directory = dirname($load);
+                $load = basename($load) . '.' . Autoload::EXT_PHP;
+                $load_compile = Autoload::name_reducer(
+                    $object,
+                    $load,
+                    $object->config('cache.parse.url.name_length'),
+                    $object->config('cache.parse.url.name_separator'),
+                    $object->config('cache.parse.url.name_pop_or_shift')
+                );
+                if(str_contains($load_compile, '_')){
+                    $data[] = $object->config('autoload.cache.compile') . $load_compile;
+                }
+                $load = Autoload::name_reducer(
+                    $object,
+                    $load,
+                    $object->config('cache.autoload.url.name_length'),
+                    $object->config('cache.autoload.url.name_separator'),
+                    $object->config('cache.autoload.url.name_pop_or_shift')
+                );
+                $load_directory = Autoload::name_reducer(
+                    $object,
+                    $load_directory,
+                    $object->config('cache.autoload.url.directory_length'),
+                    $object->config('cache.autoload.url.directory_separator'),
+                    $object->config('cache.autoload.url.directory_pop_or_shift')
+                );
+                $load_url = $object->config('autoload.cache.class') . $load_directory . '_' . $load;
+                $data[] = $load_url;
+                $object->config('autoload.cache.file.name', $load_url);
             }
-            $load = Autoload::name_reducer(
-                $object,
-                $load,
-                $object->config('cache.autoload.url.name_length'),
-                $object->config('cache.autoload.url.name_separator'),
-                $object->config('cache.autoload.url.name_pop_or_shift')
-            );
-            $load_directory = Autoload::name_reducer(
-                $object,
-                $load_directory,
-                $object->config('cache.autoload.url.directory_length'),
-                $object->config('cache.autoload.url.directory_separator'),
-                $object->config('cache.autoload.url.directory_pop_or_shift')
-            );
-            $load_url = $object->config('autoload.cache.class') . $load_directory . '_' . $load;
-            $data[] = $load_url;
-            $object->config('autoload.cache.file.name', $load_url);
         }
         if(
             property_exists($this->read, 'autoload') &&
@@ -523,24 +525,27 @@ class Autoload {
         ){
             $data[] = $this->read->autoload->{$caller}->{$item['load']};
         }
-        $item['file_dot'] = str_replace('_', '.', $item['file']);
-        $data[] = $item['directory'] . $item['file_dot'] . DIRECTORY_SEPARATOR . $item['file_dot'] . '.' . Autoload::EXT_PHP;
-        $data[] = $item['directory'] . $item['file'] . DIRECTORY_SEPARATOR . $item['file'] . '.' . Autoload::EXT_PHP;
-        $data[] = $item['directory'] . $item['file_dot'] . DIRECTORY_SEPARATOR . str_replace('_', '.', $item['baseName']) . '.' . Autoload::EXT_PHP;
-        $data[] = $item['directory'] . $item['file'] . DIRECTORY_SEPARATOR . $item['baseName'] . '.' . Autoload::EXT_PHP;
-        $explode = explode('.', $item['file_dot'], 2);
-        if(
-            $explode[0] !== $item['file_dot'] &&
-            $explode[0] !== $item['file']
-        ){
-            $data[] = $item['directory'] . $explode[0] . DIRECTORY_SEPARATOR . $item['file_dot'] . '.' . Autoload::EXT_PHP;
-            $data[] = $item['directory'] . $explode[0] . DIRECTORY_SEPARATOR . $item['file'] . '.' . Autoload::EXT_PHP;
-            $data[] = $item['directory'] . $explode[0] . DIRECTORY_SEPARATOR . str_replace('_', '.', $item['baseName']) . '.' . Autoload::EXT_PHP;
-            $data[] = $item['directory'] . $explode[0] . DIRECTORY_SEPARATOR . $item['baseName'] . '.' . Autoload::EXT_PHP;
+        foreach($item['directory'] as $nr => $directory){
+            $item['file_dot'] = str_replace('_', '.', $item['file']);
+            $data[] = $directory . $item['file_dot'] . DIRECTORY_SEPARATOR . $item['file_dot'] . '.' . Autoload::EXT_PHP;
+            $data[] = $directory . $item['file'] . DIRECTORY_SEPARATOR . $item['file'] . '.' . Autoload::EXT_PHP;
+            $data[] = $directory. $item['file_dot'] . DIRECTORY_SEPARATOR . str_replace('_', '.', $item['baseName']) . '.' . Autoload::EXT_PHP;
+            $data[] = $directory . $item['file'] . DIRECTORY_SEPARATOR . $item['baseName'] . '.' . Autoload::EXT_PHP;
+            $explode = explode('.', $item['file_dot'], 2);
+            if(
+                $explode[0] !== $item['file_dot'] &&
+                $explode[0] !== $item['file']
+            ){
+                $data[] = $directory . $explode[0] . DIRECTORY_SEPARATOR . $item['file_dot'] . '.' . Autoload::EXT_PHP;
+                $data[] = $directory . $explode[0] . DIRECTORY_SEPARATOR . $item['file'] . '.' . Autoload::EXT_PHP;
+                $data[] = $directory . $explode[0] . DIRECTORY_SEPARATOR . str_replace('_', '.', $item['baseName']) . '.' . Autoload::EXT_PHP;
+                $data[] = $directory . $explode[0] . DIRECTORY_SEPARATOR . $item['baseName'] . '.' . Autoload::EXT_PHP;
+            }
+            $data[] = $directory . $item['file_dot'] . '.' . Autoload::EXT_PHP;
+            $data[] = $directory . $item['file'] . '.' . Autoload::EXT_PHP;
+            $data[] = $directory . $item['baseName'] . '.' . Autoload::EXT_PHP;
         }
-        $data[] = $item['directory'] . $item['file_dot'] . '.' . Autoload::EXT_PHP;
-        $data[] = $item['directory'] . $item['file'] . '.' . Autoload::EXT_PHP;
-        $data[] = $item['directory'] . $item['baseName'] . '.' . Autoload::EXT_PHP;
+
         $this->fileList[$item['baseName']][] = $data;
         $result = [];
         foreach($data as $nr => $file){
