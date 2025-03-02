0a30afdf3f089dc31e5b4a0a8426f260ef8cec7c
remco van der Velde <remco.pc@outlook.com>
2025-03-02 13:24:08 +0100

    make functions like new parser (traits)

---
+++ src/Cli/Info/Plugin/Info.All.Add.php
@@ -0,0 +1,55 @@
+<?php
+namespace Plugin;
+
+use Exception;
+
+use Raxon\Config;
+use Raxon\Module\Core;
+use Raxon\Module\Dir;
+use Raxon\Module\File;
+use Raxon\Module\Parse;
+
+use Raxon\Cli\Cache\Controller\Cache;
+
+trait Info_All_Add
+{
+
+    /**
+     * @throws Exception
+     */
+    protected function info_all_add(array $list =[]): array
+    {
+        $object = $this->object();
+        $result = [];
+        foreach($list as $nr => $record){
+            if(
+                property_exists($record, 'controller') &&
+                property_exists($record, 'function')
+            ){
+                try {
+                    $class = $record->controller;
+                    $constant =  $class . '::INFO_' . strtoupper($record->function);
+                    $info = false;
+                    if(defined($constant)) {
+                        $info = constant($constant);
+                    }
+                    elseif(defined($class . '::INFO')){
+                        $info = constant($class . '::INFO');
+                    }
+                    $record->info = $info;
+                    $result[] = $record;
+                } catch (Exception $exception){
+                    Cli::tput('init');
+                    echo Cli::tput('background', CLI::COLOR_RED);
+                    echo PHP_EOL;
+                    echo PHP_EOL;
+                    echo $exception->getMessage() . PHP_EOL;
+                    echo Cli::tput('reset');
+                    echo PHP_EOL;
+                    continue;
+                }
+            }
+        }
+        return $result;
+    }
+}
