cc9d44b0504f308b82811a04b29b3402853ebed4
remco van der Velde <remco.pc@outlook.com>
2025-03-02 19:59:15 +0100

    make functions like new parser (traits)

--- src/Cli/Install/Controller/Install.php
+++ src/Cli/Install/Controller/Install.php
@@ -72,7 +72,6 @@ class Install extends Controller {
             $url,
             'package.' . $key
         );
-        ddd($package);
         if($package->has('composer')){
             Dir::change($object->config('project.dir.root'));
             Core::execute($object, $package->get('composer'), $output, $notification);
