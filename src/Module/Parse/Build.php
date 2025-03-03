f183c23bfbb209bbe554e75ccd8b470b252ff3d6
remco van der Velde <remco.pc@outlook.com>
2025-03-03 20:24:21 +0100

    make functions like new parser (traits)

--- src/Module/Parse/Build.php
+++ src/Module/Parse/Build.php
@@ -689,7 +689,7 @@ class Build {
         $is_control = false;
         $remove_newline = false;
         foreach($tree as $nr => $record){
-            d($record['parse']);
+            d($record['parse'] ?? $record);
             $start = microtime(true);
             if(
                 $skip_nr !== null &&
