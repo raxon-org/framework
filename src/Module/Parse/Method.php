4831ba82d41d9c4a2ef537c11d5c8f46a4a64e73
remco van der Velde <remco.pc@outlook.com>
2025-03-03 20:36:39 +0100

    make functions like new parser (traits)

--- src/Module/Parse/Method.php
+++ src/Module/Parse/Method.php
@@ -390,6 +390,7 @@ class Method {
                                 }
                             }
                             if($is_found === false){
+                                d($build->object()->config('package.raxon/parse'));
                                 d($build->storage());
                                 throw new LocateException('Plugin (' . $item['namespace'] . $item['name'] . ') not found...', $location);
                             }
