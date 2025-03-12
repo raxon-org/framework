71ce5016f30b91c74b3e0ad49cbe455c3afb1466
remco van der Velde <remco.pc@outlook.com>
2025-03-12 15:10:40 +0100

    init

--- src/Cli/Install/Controller/Install.php
+++ src/Cli/Install/Controller/Install.php
@@ -78,6 +78,7 @@ class Install extends Controller {
             $url,
             'package.' . $key
         );
+        ddd($package);
         if($package->has('composer')){
             Dir::change($object->config('project.dir.root'));
             Core::execute($object, $package->get('composer'), $output, $notification);
