969c6bb72c29013b9a069029b2df52b3d50b2b2f
remco van der Velde <remco.pc@outlook.com>
2025-03-21 11:48:26 +0100

    sqlite

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -344,7 +344,7 @@ class Core
             throw new UrlEmptyException('url is empty...');
         }
         header('Location: ' . $url, true, 302);
-        d('yes');
+        d($url);
         exit;
     }
 
