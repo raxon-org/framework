78448d74ccffa50515233a43d8edbd84ea46ed2e
remco van der Velde <remco.pc@outlook.com>
2025-04-24 19:06:31 +0200

    implementing new parser

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -122,8 +122,6 @@ class Core
      */
     public static function execute(App $object, string $command, string &$output = null, string &$notification = null, string $type = null): mixed
     {
-        trace();
-        ddd('strange');
         if ($output === null) {
             $output = '';
         }
