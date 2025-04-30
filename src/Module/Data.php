7df264192137ad0a145e41fdb75e38675897499d
remco van der Velde <remco.pc@outlook.com>
2025-04-30 16:16:23 +0200

    implementing new parser

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -271,7 +271,7 @@ class Data {
             $attribute .= '.' . $this->index($attribute);
 //            d($attribute);
         }
-        $explode = explode('[', $attribute);
+        $explode = explode('[', $attribute, 2);
         if(array_key_exists(1, $explode)){
             ddd($explode);
         }
