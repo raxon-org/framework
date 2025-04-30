6e96c6f50165709af84f541234886f72278aba50
remco van der Velde <remco.pc@outlook.com>
2025-04-30 17:42:14 +0200

    implementing new parser

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -273,6 +273,7 @@ class Data {
         }
         $explode = explode('[', $attribute, 2);
         if(array_key_exists(1, $explode)){
+            $attribute = $explode[0];
             $temp = explode('][', substr($explode[1], 0, -1));
             foreach($temp as $nested_attribute_key){
                 $nested_attribute_value = $this->get($attribute . '.' . $nested_attribute_key);
