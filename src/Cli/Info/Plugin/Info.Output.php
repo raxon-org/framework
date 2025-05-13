08c1b4403c9b1b279b22075ba506c9138c8705cc
remco van der Velde <remco.pc@outlook.com>
2025-05-13 16:21:21 +0200

    info output

--- src/Cli/Info/Plugin/Info.Output.php
+++ src/Cli/Info/Plugin/Info.Output.php
@@ -4,6 +4,7 @@ namespace Plugin;
 use Exception;
 
 use Raxon\Module\Core;
+use Raxon\Module\Data;
 
 trait Info_Output
 {
@@ -40,11 +41,58 @@ trait Info_Output
         }
         foreach($command as $nr => $line){
             $counter = $nr + 1;
-            echo '[' . $counter . ']' . str_replace('{{binary()}}', Core::binary($object), $line . PHP_EOL);
+            echo '[' . $counter . ']' . $this->info_parse_string($line . PHP_EOL);
         }
         foreach($description as $nr => $line){
             $counter = $nr + 1;
-            echo '[' . $counter . ']' . $line . PHP_EOL;
+            echo '[' . $counter . ']' . $this->info_parse_string($line . PHP_EOL);
         }
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function info_parse_string(mixed $mixed, array|object $data = null): mixed
+    {
+        $parse = $this->parse();
+
+        if($data === null){
+            $data = $this->data();
+        }
+        elseif(is_array($data)){
+            $data = new Data($data);
+        }
+        elseif(is_object($data)) {
+            if(get_class($data) === Data::class){
+                $data = $data;
+            } else {
+                $data = new Data($data);
+            }
+        }
+        $options = $parse->options();
+        $old_source = $options->source ?? null;
+        if(is_scalar($mixed) || is_null($mixed)){
+            if(is_string($mixed)){
+                $hash = 'scalar_' . hash('sha256', '{"scalar": "' . $mixed . '"}');
+            } else {
+                $hash = 'scalar_' . hash('sha256', '{"scalar":' . $mixed . '}');
+            }
+        } else {
+            $hash = hash('sha256', Core::object($mixed, Core::JSON_LINE));
+        }
+        $options->source = 'Internal_' . $hash;
+        $parse->options($options);
+        if(!empty($parseData)){
+            $result = $parse->compile($mixed, $data);
+        } else {
+            $result = $parse->compile($mixed, []);
+        }
+        if($old_source !== null){
+            $options->source = $old_source;
+        } else {
+            unset($options->source);
+        }
+        $parse->options($options);
+        return $result;
     }
 }
