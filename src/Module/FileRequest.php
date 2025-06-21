304bf73ee8324ba79cfde80e638ce775feca1a21
remco van der Velde <remco.pc@outlook.com>
2025-06-22 00:01:15 +0200

    php 8.4 deprecations

--- src/Module/FileRequest.php
+++ src/Module/FileRequest.php
@@ -172,8 +172,9 @@ class FileRequest {
         $logger_error = $object->config('project.log.error');
         $request = $object->data(App::REQUEST);
         $input = $request->data('request');
-        d($request)
-        ddd($input);
+        var_dump($request)
+        var_dump($input);
+        die;
         $dir = false;
         $file = false;
         if(!in_array($input, [null, ''], true)){
