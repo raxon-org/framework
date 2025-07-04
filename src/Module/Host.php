a4dc624523f51ad3315172d975e5520bcfda0085
remco van der Velde <remco.pc@outlook.com>
2025-07-04 19:36:22 +0200

    Add debugging statement to log host information in map method

--- src/Module/Host.php
+++ src/Module/Host.php
@@ -165,6 +165,7 @@ class Host {
         }
         $map = Host::map($object, $node, $name);
         $host = Host::get($object, $node, $name, $map);
+        ddd($host);
         $object->config('host.map', $map);
         $object->config('host', Core::object_merge($object->config('host'), $host));        
         return true;
