87effc2d7b4a16f2589eb90bb9ca7fb11649bcc9
remco van der Velde <remco.pc@outlook.com>
2025-08-02 14:29:46 +0200

    reset

--- src/Cli/Reset/Plugin/Reset.php
+++ src/Cli/Reset/Plugin/Reset.php
@@ -18,7 +18,31 @@ trait Reset
      */
     protected function reset(object $flags, object $options): void
     {        
-        d($flags);
-        d($options);
+        if(property_exists($options, 'app')){
+            $options->application = $options->app;
+            unset($options->app);
+        }
+        if(property_exists($options, 'application') && !empty($options->application)){
+            $name = $options->application;
+            //frankenphp  init ?
+            $commands = [
+                'app raxon/basic apache2 setup',
+                'app raxon/basic apache2 restore',
+                'app raxon/basic apache2 restart',
+                'app raxon/basic cron restore',
+                'app raxon/basic cron restart',
+                'app raxon/basic php restore',
+                'app raxon/basic php restart',
+            ];
+            $object = $this->object();
+            foreach($commands as $command){
+                Core::execute($object, $command, $output, $notification);
+                if($output){
+                    echo $output;
+                }
+                if($notification){
+                    echo $notification;
+                }
+            }
     }
 }
