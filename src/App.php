4f348fd52b2f12706588b94fd866237959b72d55
Remco van der Velde <remco@universeorange.com>
2026-04-21 17:19:44 +0200

    x

--- src/App.php
+++ src/App.php
@@ -104,6 +104,7 @@ class App extends Data {
         App::is_cli();
         require_once __DIR__ . '/Debug.php';
         require_once __DIR__ . '/Error.php';
+        require_once __DIR__ . '/Utils.php';
         Config::configure($this);
         Logger::configure($this);
 //        Host::configure($this);
