ee1724f05d351042efb103d87b062901e7880869
Remco van der Velde <remco@universeorange.com>
2026-04-26 20:38:49 +0200

    init

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -84,7 +84,7 @@ class Controller {
         ){
             $before = File::basename($before);
         }
-//        $name = str_replace(':','.', $name); // @off @ 2026 04 26
+        $name = str_replace(':','.', $name);
         $name = Core::ucfirst_sentence(str_replace('_','.', $name));
         if($before !== null){
             return $before . $delimiter . $name;
