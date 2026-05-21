44621a140c4fbf4405745c5954279e0529c0e2a6
Remco van der Velde <remco@universeorange.com>
2026-05-21 17:40:42 +0200

    init

--- src/Module/FileRequest.php
+++ src/Module/FileRequest.php
@@ -155,7 +155,6 @@ class FileRequest {
      */
     public static function get(App $object): string | bool
     {
-        ddd('test');
         if (
             array_key_exists('REQUEST_METHOD', $_SERVER) &&
             $_SERVER['REQUEST_METHOD'] == 'OPTIONS'
