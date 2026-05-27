b93a0ba3f5616527ae646467952efb0832d66aa7
Remco van der Velde <remco@universeorange.com>
2026-05-27 20:30:48 +0200

    init

--- src/App.php
+++ src/App.php
@@ -187,9 +187,6 @@ class App extends Data {
                 App::configure($object);
                 Route::configure($object);
                 $destination = Route::request($object);
-                if(!defined('IS_CLI')){
-                    dd($destination);
-                }
                 if ($destination === false) {                    
                     if ($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
                         if($logger){
