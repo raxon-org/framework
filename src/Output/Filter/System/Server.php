f6e709e672e15fcf27ca26b8068caa3a392a9357
Remco van der Velde <remco@universeorange.com>
2026-05-31 22:05:30 +0200

    init

--- src/Output/Filter/System/Server.php
+++ src/Output/Filter/System/Server.php
@@ -48,7 +48,48 @@ class Server extends Controller {
 
     public static function output_filter(App $object, $response=null): object
     {
-        dd($response);
+        foreach($response as $nr => $record){
+            if(property_exists($record, 'extension')){
+                $extension_list = [];
+                $is_extension_list = false;
+                foreach($record->extension as $extension){
+                    if(
+                        property_exists($extension, '#class') &&
+                        property_exists($extension, 'uuid') &&
+                        property_exists($extension, 'extension') &&
+                        property_exists($extension, 'file_extension')
+                    ){
+                        if(!in_array($extension->extension, $extension_list, true)){
+                            $extension_list[$extension->extension] = $extension->file_extension;
+                            $is_extension_list = true;
+                        }
+                    }
+                }
+                if($is_extension_list){
+                    $record->extension = $extension_list;
+                }
+            }
+            if(property_exists($record, 'contentType')){
+                $content_type_list = [];
+                $is_content_type_list = false;
+                foreach($record->contentType as $contentType){
+                    if(
+                        property_exists($contentType, '#class') &&
+                        property_exists($contentType, 'uuid') &&
+                        property_exists($contentType, 'extension') &&
+                        property_exists($contentType, 'content_type')
+                    ){
+                        if(!in_array($contentType->extension, $content_type_list, true)){
+                            $content_type_list[$extension->extension] = $contentType->content_type;
+                            $is_content_type_list = true;
+                        }
+                    }
+                }
+                if($is_content_type_list){
+                    $record->contentType = $content_type_list;
+                }
+            }
+        }
         return $response;
     }
 
