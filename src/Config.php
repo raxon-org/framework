369fafc93b8ae8bc7cd0070a31d555eab9908b14
Remco van der Velde <remco@universeorange.com>
2026-05-31 23:07:27 +0200

    init

--- src/Config.php
+++ src/Config.php
@@ -572,11 +572,10 @@ class Config extends Data {
             'ramdisk_dir' => $dir_cache,
         ];
         $role_system = $node->role_system();
-        $role_system = false;
         if(!$role_system){
             throw new Exception('System role not found.');
         }
-        if(!$node->role_has_permission($role_system, 'System:Config:record')){
+        if(!$node->role_has_permission($role_system, 'System:Config:record2')){
             throw new Exception('System has not this permission: System:Config:record');
         }        
         $response = $node->record($class, $role_system, $options);        
