345f9a8a4a381b2053a2cab3bf0481bf868e8465
Remco van der Velde <remco@universeorange.com>
2026-06-10 16:00:02 +0200

    init

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -746,7 +746,7 @@ class Controller {
         $parse = new Parse($object, $data, $flags, $options);
 //        Controller::decorate($object);
         $read = $parse->compile($read, $data);
-        if($data>get('contentType') !== 'text/cli'){
+        if($data->get('contentType') !== 'text/cli'){
             d($data);
         }
         Parse::readback($object, $parse, App::SCRIPT);
