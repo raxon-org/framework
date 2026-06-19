0c2bc449f27e1e0669dd42b861428a442c8753ba
Remco van der Velde <remco@universeorange.com>
2026-06-19 16:00:30 +0200

    init

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -2222,4 +2222,75 @@ class Core
         }
         return $codepoints;
     }
+
+    public static function time_format(float|int $seconds=0, string $string='in', $compact=false): string
+    {
+        $days = floor($seconds / (3600 * 24));
+        $hours = floor(($seconds / 3600) % 24);
+        $minutes = floor(($seconds / 60) % 60);
+        $explode = explode('.', $seconds);
+        $msec = $explode[1] ?? 0;
+        $seconds = $seconds % 60;
+        if($days > 0){
+            if($compact){
+                $string .= $days . ' ' . 'd' . ' ';
+            } else {
+                if($days === 1){
+                    $string .= $days . ' ' . 'day' . ' ' . 'and' . ' ';
+                } else {
+                    $string .= $days . ' ' . 'days' . ' ' . 'and' . ' ';
+                }
+            }
+        }
+        if($hours > 0){
+            if($compact){
+                $string .= $hours . ' ' . 'h' . ' ';
+            } else {
+                if($hours === 1){
+                    $string .= $hours . ' ' . 'hour' . ' ' . 'and' . ' ';
+                } else {
+                    $string .= $hours . ' ' . 'hours' . ' ' . 'and' . ' ';
+                }
+            }
+        }
+        if ($minutes > 0){
+            if($compact){
+                $string .= $minutes . ' ' . 'min' . ' ';
+            } else {
+                if($minutes === 1){
+                    $string .= $minutes . ' ' . 'minute' . ' ' . 'and' . ' ';
+                } else {
+                    $string .= $minutes . ' ' . 'minutes' . ' ' . 'and' . ' ';
+                }
+            }
+
+        }
+        if($seconds < 1){
+            if($days === 0 && $hours === 0 && $minutes === 0){
+                if($compact){
+                    $string = substr($msec, 0, 3) * 1000 . ' ' . 'msec';
+                } else {
+                    $string = 'almost there';
+                }
+            } else {
+                if($compact){
+                    $string .= $seconds . '.' . substr($msec, 0, 3) . ' sec';
+                } else {
+                    $string .= $seconds . '.' . substr($msec, 0, 3) .  ' seconds';
+                }
+            }
+
+        } else {
+            if($compact){
+                $string .= $seconds . '.' . substr($msec, 0, 3) . ' sec';
+            } else {
+                if($seconds === 1){
+                    $string .= $seconds . '.' . substr($msec, 0,3) . ' second';
+                } else {
+                    $string .= $seconds . '.' . substr($msec, 0, 3) . ' seconds';
+                }
+            }
+        }
+        return $string;
+    }
 }
