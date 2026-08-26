40140606b8eeca5dd7dc9d19703160e14dbbe131
Remco van der Velde <remco@universeorange.com>
2026-08-26 20:23:01 +0200

    init

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -277,7 +277,7 @@ class Controller {
                 $name
             );
             $list[] = $dir . $name . $config->data('extension.tpl');
-            d($object->config('extension.tpl'));
+            d($object->config());
             d($config->data('extension.tpl'));
             ddd($list);
             if(!empty($object->config('controller.dir.view'))){
