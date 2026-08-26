af65eafa5749582b16d7159f36dbf0bdcc47f0c9
Remco van der Velde <remco@universeorange.com>
2026-08-26 18:45:24 +0200

    init

--- src/Cli/Ramdisk/Controller/Ramdisk.php
+++ src/Cli/Ramdisk/Controller/Ramdisk.php
@@ -58,6 +58,8 @@ class Ramdisk extends Controller {
                     throw $exception;
             }
             if($name){
+                d($name);
+                d($command);
                 $url = Ramdisk::locate($object, $name);
                 d($name);
                 d($url);
