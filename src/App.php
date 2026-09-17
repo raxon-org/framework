7939f3364480b2c8e5b76efd237522ef8827a597
Remco van der Velde <remco@universeorange.com>
2026-09-17 12:18:59 +0200

    init

--- src/App.php
+++ src/App.php
@@ -2010,7 +2010,7 @@ class App extends Data {
             if($attribute !== null && $cache){
                 $cache->set($attribute, $data);
             }
-            dd($data);
+            d($data);
             return $data;
         } else {
             return false;
