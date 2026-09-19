1bdbd2f5daff5ffc46ebddc156795835804eb9a2
Remco van der Velde <remco@universeorange.com>
2026-09-19 19:13:34 +0200

    init

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -394,6 +394,7 @@ class Controller {
         }
         foreach($list as $file){
             if(File::exist($file)){
+                d('yes');
                 if(
                     $object->config('ramdisk.url') &&
                     !empty($object->config('ramdisk.is.disabled')) &&
